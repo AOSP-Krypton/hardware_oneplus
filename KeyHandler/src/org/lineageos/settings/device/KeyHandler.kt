7e3bcb5f0e239288b0e992cfc8e10674a009102d
jhonboy121 <alfredmathew05@gmail.com>
2021-10-15 18:31:39 +0530

    KeyHandler: adapt to fwb changes
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -16,23 +16,26 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
     private val audioManager = context.getSystemService(AudioManager::class.java)
     private val vibrator = context.getSystemService(Vibrator::class.java)
 
-    override fun handleKeyEvent(event: KeyEvent): KeyEvent {
+    override fun handleKeyEvent(event: KeyEvent): Boolean {
         if (event.action == KeyEvent.ACTION_DOWN) {
             when (event.scanCode) {
                 MODE_NORMAL -> {
                     audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL)
                     vibrator.vibrate(MODE_NORMAL_EFFECT)
+                    return true
                 }
                 MODE_VIBRATION -> {
                     audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE)
                     vibrator.vibrate(MODE_VIBRATION_EFFECT)
+                    return true
                 }
                 MODE_SILENCE -> {
                     audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT)
+                    return true
                 }
             }
         }
-        return event
+        return false
     }
 
     companion object {
