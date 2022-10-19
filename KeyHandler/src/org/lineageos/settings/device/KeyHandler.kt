d05ed2b9616711eb6316df5590339394a491a22e
LuK1337 <priv.luk@gmail.com>
2022-10-19 20:42:54 +0200

    KeyHandler: Use VibrationAttributes.USAGE_HARDWARE_FEEDBACK
    This fixes vibration when user disabled 'Touch feedback' in settings.
    
    Change-Id: If465f34c86a1e8e1b0f0b2ee3cc74515315e30b4
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -7,6 +7,7 @@ package org.lineageos.settings.device
 
 import android.content.Context
 import android.media.AudioManager
+import android.os.VibrationAttributes
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.KeyEvent
@@ -22,11 +23,18 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
                 MODE_NORMAL -> {
                     audioManager.ringerModeInternal = AudioManager.RINGER_MODE_NORMAL
                     vibrator.vibrate(MODE_NORMAL_EFFECT)
+                    vibrator.vibrate(
+                        MODE_NORMAL_EFFECT,
+                        HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES
+                    )
                     return true
                 }
                 MODE_VIBRATION -> {
                     audioManager.ringerModeInternal = AudioManager.RINGER_MODE_VIBRATE
-                    vibrator.vibrate(MODE_VIBRATION_EFFECT)
+                    vibrator.vibrate(
+                        MODE_VIBRATION_EFFECT,
+                        HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES
+                    )
                     return true
                 }
                 MODE_SILENCE -> {
@@ -45,6 +53,10 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         private const val MODE_NORMAL = 601
         private const val MODE_VIBRATION = 602
         private const val MODE_SILENCE = 603
+
+        // Vibration attributes
+        private val HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES =
+            VibrationAttributes.createForUsage(VibrationAttributes.USAGE_HARDWARE_FEEDBACK)
 
         // Vibration effects
         private val MODE_NORMAL_EFFECT = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK)
